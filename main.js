d0c3abed4f4aa49622e3a8fe2566ff236431b5b9
hepengshuai <hepengshuai2016@gmail.com>
2024-07-03 10:12:53 +0800

    check port

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 import {add, runWeb} from './api/web.js'
+import net
 
 const n = add(1, 2)
 console.log('The sum is ' + n);
 
-runWeb(3002)
+function randomNumByRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const tryUsePort = async function(port, portAvailableCallback){
+    function portUsed(port){
+        return new Promise((resolve, reject)=>{
+            let server = net.createServer().listen(port);
+            server.on('listening',function(){
+                server.close();
+                resolve(port);
+            });
+            server.on('error',function(err){
+                if(err.code == 'EADDRINUSE'){
+                    resolve(err);
+                }
+            });             
+        });
+    }
+ 
+    let res = await portUsed(port);
+    if(res instanceof Error){
+        console.log(`端口：${port}被占用\n`);
+        port = randomNumByRange(3000, 4000)
+        tryUsePort(port, portAvailableCallback);
+    }else{
+        portAvailableCallback(port);
+    }
+}
+
+tryUsePort(3000, (port) => {
+    web.listen(port, () => console.log('Web server running on port', port));
+})
