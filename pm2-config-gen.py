ed2b978a994804c1dc981acc4bd342b7f15fcc3e
hepengshuai <hepengshuai2016@gmail.com>
2024-07-04 11:06:24 +0800

    generate nginx config

--- pm2-config-gen.py
+++ pm2-config-gen.py
@@ -30,6 +30,34 @@ def generate_deploy_config():
         file.write(output)
 
 
+def generate_nginx_config():
+    # 创建一个 Jinja2 的 Environment 对象，指定模板文件所在的文件夹
+    env = Environment(loader=FileSystemLoader('.'))
+
+    # 定义要传递给模板的数据
+    data = {
+        'ip': '1.1.1.1'
+    }
+
+    items = []
+    port = 56201
+    for i in range(201, 401):
+        items.append({
+            "port": port
+        })
+        port = port + 1
+
+    data['items'] = items
+
+    # 渲染模板并获取渲染后的结果
+    template = env.get_template('nginx-conf.template')
+    output = template.render(data)
+
+    # 将渲染后的结果写入到一个 HTML 文件中
+    with open('server.conf', 'w') as file:
+        file.write(output)
+
+
 def copy_user_data():
     source_dir = '/data/tmp/'
     dest_dir = '/data1/tmp/'
